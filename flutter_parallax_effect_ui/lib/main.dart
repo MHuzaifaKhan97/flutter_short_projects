4d6953cec35df7b301155f9b551b5d8f40a13578
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2022-09-12 22:44:23 +0500

    Assets and Icons added.

--- flutter_parallax_effect_ui/lib/main.dart
+++ flutter_parallax_effect_ui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_parallax_effect_ui/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,19 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: HomeScreen(),
     );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
