81338aca4cb2c5887c3f057315a5823500c5330e
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-09-12 23:40:57 +0500

    Update font

--- flutter_parallax_effect_ui/lib/main.dart
+++ flutter_parallax_effect_ui/lib/main.dart
@@ -11,11 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Buy Tickets',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(fontFamily: 'SF Pro Display'),
       home: HomeScreen(),
     );
   }
