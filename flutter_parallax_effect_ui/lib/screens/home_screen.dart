be038b375bdef78f0e99f91fd1c29e9576315ce5
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-09-12 23:39:25 +0500

    Exhibition, Scrollable Exhibition Bottom

--- flutter_parallax_effect_ui/lib/screens/home_screen.dart
+++ flutter_parallax_effect_ui/lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_parallax_effect_ui/screens/exhibitiion_bottom_sheet.dart';
+import 'package:flutter_parallax_effect_ui/screens/sliding_card.dart';
+import 'package:flutter_parallax_effect_ui/screens/tabs.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,7 +9,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Hello World")),
+      body: Stack(
+        children: <Widget>[
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 8),
+                Header(),
+                SizedBox(height: 40),
+                Tabs(),
+                SizedBox(height: 8),
+                SlidingCardsView(),
+              ],
+            ),
+          ),
+          ExhibitionBottomSheet(), //use this or ScrollableExhibitionSheet
+        ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 32),
+      child: Text(
+        'Buy Tickets',
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
     );
   }
 }
