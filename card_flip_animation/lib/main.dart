e0c2b5285d75adc1c9a8593ca751c47c810a5051
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2022-09-19 19:50:55 +0500

    Card flip animation

--- card_flip_animation/lib/main.dart
+++ card_flip_animation/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,16 +16,107 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: HomePage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //declare the isBack bool
+  bool isBack = true;
+  double angle = 0;
+
+  void _flip() {
+    setState(() {
+      angle = (angle + pi) % (2 * pi);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF292a3e),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Now we will start making our Card
+              //the principe is basic
+              //we will create a container that will display the back face or the front face
+              // and we will wrap it in a Transform widget
+              //then we will make a tween animation for animating our card
+              //now let's wrap our tween animation inside a gesture detector
+
+              GestureDetector(
+                onTap: _flip,
+                child: TweenAnimationBuilder(
+                    tween: Tween<double>(begin: 0, end: angle),
+                    duration: Duration(seconds: 1),
+                    builder: (BuildContext context, double val, __) {
+                      //here we will change the isBack val so we can change the content of the card
+                      if (val >= (pi / 2)) {
+                        isBack = false;
+                      } else {
+                        isBack = true;
+                      }
+                      return (Transform(
+                        //let's make the card flip by it's center
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.001)
+                          ..rotateY(val),
+                        child: Container(
+                            width: 309,
+                            height: 474,
+                            child: isBack
+                                ? Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      // borderRadius: BorderRadius.circular(10.0),
+                                      // image: DecorationImage(
+                                      //   image: AssetImage("assets/back.png"),
+                                      // ),
+                                    ),
+                                  ) //if it's back we will display here
+                                : Transform(
+                                    alignment: Alignment.center,
+                                    transform: Matrix4.identity()
+                                      ..rotateY(
+                                          pi), // it will flip horizontally the container
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.orange,
+
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        // image: DecorationImage(
+                                        //   // image: AssetImage("assets/face.png"),
+                                        // ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          "Surprise ! 🎊",
+                                          style: TextStyle(
+                                            fontSize: 30.0,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ) //else we will display it here,
+                            ),
+                      ));
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
