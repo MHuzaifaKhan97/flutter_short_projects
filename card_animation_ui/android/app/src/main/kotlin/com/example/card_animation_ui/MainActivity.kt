a0a9200ed4dad6c53780f0bd377067e47deac726
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2022-09-21 18:48:09 +0500

    init

---
+++ card_animation_ui/android/app/src/main/kotlin/com/example/card_animation_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_animation_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ card_animation_ui/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:card_animation_ui/screen/card_detail.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CardDetailScreen(),
+    );
+  }
+}
