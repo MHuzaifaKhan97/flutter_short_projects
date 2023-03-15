815f72959c55c8b983738f009d326276c60fd93e
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2023-03-16 01:49:18 +0500

    Simple add using hive data

--- flutter_hive_crud/lib/home_screen.dart
+++ flutter_hive_crud/lib/home_screen.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:hive/hive.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Hive CRUD")),
+      body: Column(
+        children: [
+          FutureBuilder(
+              future: Hive.openBox('todos'),
+              builder: ((BuildContext context, AsyncSnapshot<Box> snapshot) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(snapshot.data?.get('name')),
+                    Text(snapshot.data!.get('age').toString()),
+                    Text(snapshot.data!.get('details').toString()),
+                    Text(snapshot.data!.get('details')["lang"].toString()),
+                    Text(snapshot.data!.get('details')["prog-lang"].toString()),
+                  ],
+                );
+              }))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var box = await Hive.openBox('todos');
+          box.put('name', "Muhammad Huzaifa Khan");
+          box.put('age', 25);
+          box.put('details', {
+            "lang": ["English", "Urdu"],
+            "prog-lang": ["Flutter", 'Dart', "Node", "React"]
+          });
+          print(box.get('name'));
+          print(box.get('age'));
+          print(box.get('details'));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
--- flutter_hive_crud/lib/main.dart
+++ flutter_hive_crud/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hive_crud/home_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //Hive initialization
+  var directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
   runApp(const MyApp());
 }
 
@@ -12,10 +18,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Hive CRUD',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
