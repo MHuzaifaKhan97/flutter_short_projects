9382fddd1af2ed76bd41281f81420af742f49e18
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-04-12 12:10:52 +0500

    Dotenv & Dart Define init.

---
+++ env_dart_defines_example/android/app/src/main/kotlin/com/example/env_dart_defines_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.env_dart_defines_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_flavor_work/android/app/src/main/kotlin/com/example/flutter_flavor_work/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_flavor_work
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
